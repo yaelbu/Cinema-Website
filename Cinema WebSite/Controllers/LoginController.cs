a9d7e0f8c308c0d9d15535ede82021519850b018
YAEL BUISINE <yaelbu11@ac.sce.ac.il>
2020-10-26 01:34:57 +0200

    Login

---
+++ Cinema WebSite/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Cinema_WebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema_WebSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoginHomePage() { 
+
+            return View();
+        }
+
+        public ActionResult Submit()
+        {
+            UsersLogin uslogin = new UsersLogin();
+            uslogin.UserName = Request.Form["UserName"];
+            uslogin.Password = Request.Form["Password"];
+
+
+            return View("PrintDetails",uslogin);
+        }
+    }
+}
---
+++ Cinema WebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cinema_WebSite.MvcApplication" Language="C#" %>
